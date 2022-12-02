b3b0ea7106ea8861a0fbeb7f62c7c453a8070a28
Thomas Pondelek <thomas.pondelek@page-effect.com>
2022-12-02 18:37:08 +0100

    liste modifiziert, meta field haupteintrag ergäntzt

--- import_list.php
+++ import_list.php
@@ -44,7 +44,7 @@ class PE_Import_Company_List_excute{
           'firmengruppen-hierarchie'  => 0,
           'firmengruppen-seite'=> 'g-u-t/',
           'Abschaltung' => '<h5>Was wir für weniger Lichtverschmutzung und weniger Energieverbrauch tun:</h5><p>Die einzelnen Partnerhäuser der G.U.T.-GRUPPE entscheiden eigenständig für ihre Niederlassungen, Ausstellungen und ABEX-Abholläger, welche Beleuchtung sie wann abschalten. Wir haben mal nachgefragt und sind bereits zu Beginn der Aktion zu einem tollen Ergebnis gekommen: <strong>Bei zwei Dritteln (65,9%) aller G.U.T.-Standorte sind um 21 Uhr die Werbelichter und alle Beleuchtungen ausgeschaltet! </strong>Knapp 16 Prozent legen den Schalter schon eine Stunde früher um. Bei immerhin noch etwas mehr als 10 Prozent aller G.U.T.-Häuser ist es bereits um 19 Uhr und bei knapp 8 Prozent bereits um 18 Uhr alles dunkel.</p><p>Aber ganz besonders stolz sind wir darauf, dass ziemlich genau die Hälfte dieser Maßnahmen bereits vor unserer Beteiligung am Projekt „22 Uhr – Licht aus“ umgesetzt waren. Wir arbeiten stetig daran, unseren Beitrag zu mehr Energieeffizienz und weniger Lichtverschmutzung zu optimieren. Für eine bessere Umwelt.</p>'
-          //'Werbebeleuchtung wurde im Projektrahmen angepasst (j/n)' =>  $vorProjekt=="ja" ? 'nein' : 'ja',
+          'Werbebeleuchtung wurde im Projektrahmen angepasst (j/n)' =>  $vorProjekt=="ja" ? 'nein' : 'nein',
                 ),
         'tax_input' => array(
           'branche' => term_exists( 'Baumärkte', 'branche'),
