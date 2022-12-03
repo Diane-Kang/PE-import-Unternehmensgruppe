f23aea9513897f8e7f945bf9643a38c0c9173dd7
Thomas Pondelek <thomas.pondelek@page-effect.com>
2022-12-03 10:01:21 +0100

    list update (München Niederlassung

--- PE-import-Unternehmensgruppe.php
+++ PE-import-Unternehmensgruppe.php
@@ -6,7 +6,7 @@
   Plugin URI: 
   Description: 
   Automatic importing when we get a list of company group.
-  Version: 0.0.1
+  Version: 0.0.2
   Author: Page-effect - Diane 
   Author URI: Page-effect.com
 */
